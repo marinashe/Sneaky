9917daaf94a047023c8cd30be322603b642a13f6
Marina Sheenkova <msheenkova@gmail.com>
2016-05-30 23:06:35 +0300

    initial commit

---
+++ display.py
@@ -0,0 +1,112 @@
+from threading import Thread
+from time import sleep
+import RPi.GPIO as IO
+
+positions = {
+    1: 3,
+    2: 5,
+    3: 7,
+    4: 11,
+    }
+
+segments = {
+    'a': 12,
+    'b': 18,
+    'c': 26,
+    'd': 24,
+    'e': 22,
+    'f': 13,
+    'g': 19,
+    }
+
+chars = {
+    '1': 'bc',
+    '2': 'abged',
+    '3': 'abcdg',
+    '4': 'fgbc',
+    '5': 'afgcd',
+    '6': 'fedcg',
+    '7': 'abc',
+    '8': 'abcdefg',
+    '9': 'abcdfg',
+    '0': 'abcdef',
+    'a': 'efgabc',
+    'b': 'fegcd',
+    'c': 'afed',
+    'd': 'bcdeg',
+    'e': 'afged',
+    'f': 'afge',
+    'g': 'afgedc',
+    'h': 'febcg',
+    'i': 'bc',
+    'j': 'bcd',
+    'l': 'fed',
+    'o': 'abcdef',
+    'p': 'fabge',
+    's': 'afgcd',
+    'u': 'fedcb',
+    '-': 'g',
+    ' ': '',
+    }
+
+
+def high(pin):
+    IO.output(pin, IO.HIGH)
+
+
+def low(pin):
+    IO.output(pin, IO.LOW)
+
+
+def clear_segments():
+    for seg in segments.values():
+        low(seg)
+
+
+def char_on(char):
+    for seg in chars[char]:
+        high(segments[seg])
+
+
+def position_on(led):
+    low(positions[led])
+
+
+def position_off(led):
+    high(positions[led])
+
+
+class Display(object):
+    def __init__(self):
+        self.value = '0'
+        self.running = True
+        for pin in positions.values() + segments.values():
+            IO.setup(pin, IO.OUT)
+        for pin in positions.values():
+            high(pin)
+        Thread(target=self.start)
+
+    def start(self):
+        try:
+            while self.running:
+                for i, c in enumerate(self.value, 1):
+                    char_on(c)
+                    position_on(i)
+                    sleep(0.002)
+                    position_off(i)
+                    clear_segments()
+
+        except KeyboardInterrupt:
+            IO.cleanup()
+
+    def set(self, x):
+        if len(str(x)) > 4:
+            raise ValueError('Invalid value for display.')
+        self.value = str(x).ljust(4, ' ')
+
+    def stop(self):
+        self.running = False
+
+
+
+
---
+++ game.py
@@ -0,0 +1,60 @@
+from threading import Thread
+from time import sleep
+import RPi.GPIO as IO
+from display import Display
+
+button = 16
+led = 19
+
+
+def high(pin):
+    IO.output(pin, IO.HIGH)
+
+
+def low(pin):
+    IO.output(pin, IO.LOW)
+
+
+class Game(object):
+    def __init__(self, display):
+        self.counter = 0
+        self.led = False
+        self.display = display
+        IO.setup(button, IO.IN, pull_up_down=IO.PUD_UP)
+        IO.setup(led, IO.OUT)
+
+        def toggle_callback(channel):
+            self.toggle()
+
+        IO.add_event_detect(button, IO.BOTH, toggle_callback, bouncetime=1000)
+
+    def timer_plus(self):
+        self.counter += 0.03
+
+    def timer_minus(self):
+        self.counter -= 0.03
+
+    def toggle(self):
+        self.led = not self.led
+        if self.led:
+            high(led)
+        else:
+            low(led)
+
+    def start(self):
+        try:
+            while True:
+                if self.led:
+                    self.timer_minus()
+                else:
+                    self.timer_plus()
+                self.display.set(int(self.counter))
+
+        except KeyboardInterrupt:
+            IO.cleanup()
+
+
+IO.setmode(IO.BOARD)
+display = Display()
+game = Game(display)
+game.start()
