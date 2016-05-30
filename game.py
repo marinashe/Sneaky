fd5767140e933161eafc911d6db16ae3a75993c6
Marina Sheenkova <msheenkova@gmail.com>
2016-05-31 00:52:46 +0300

    added game for 2 playrs

--- game.py
+++ game.py
@@ -1,10 +1,12 @@
+import sys
 from random import random, randrange
 from threading import Thread
 from time import sleep
 import RPi.GPIO as IO
 from display import Display
 
-button = 16
+button1 = 16
+button2 = 21
 led = 15
 
 
@@ -17,23 +19,27 @@ def low(pin):
 
 
 class Game(object):
-    def __init__(self, display):
+    def __init__(self, display, type_game):
         self.counter = 0
         self.led = False
         self.display = display
         self.running = True
-        IO.setup(button, IO.IN, pull_up_down=IO.PUD_UP)
+        IO.setup(button1, IO.IN, pull_up_down=IO.PUD_UP)
+        IO.setup(button2, IO.IN, pull_up_down=IO.PUD_UP)
         IO.setup(led, IO.OUT)
+        low(led)
 
         def toggle_callback(channel):
             self.toggle()
             print('Toggle')
 
-        IO.add_event_detect(button, IO.BOTH, toggle_callback, bouncetime=100)
-        Thread(target=self.comp_start).start()
+        IO.add_event_detect(button1, IO.BOTH, toggle_callback, bouncetime=100)
+        IO.add_event_detect(button2, IO.BOTH, toggle_callback, bouncetime=100)
+        if type_game == 'single':
+            Thread(target=self.comp_start).start()
 
     def timer_plus(self):
-        self.counter += 0.5
+        self.counter += 1
 
     def timer_minus(self):
         self.counter -= 1
@@ -49,19 +55,15 @@ class Game(object):
 
     def start(self):
         print('Start game')
-        try:
-            while True:
-                if self.led:
-                    self.timer_minus()
-                else:
-                    self.timer_plus()
-                self.display.set(int(self.counter))
-                print(int(self.counter))
-                sleep(0.2)
-        except KeyboardInterrupt:
-            self.comp_stop()
-            self.display.stop()
-            IO.cleanup()
+
+        while True:
+            if self.led:
+                self.timer_minus()
+            else:
+                self.timer_plus()
+            self.display.set(int(self.counter))
+            print(int(self.counter))
+            sleep(0.2)
 
     def comp_start(self):
         while self.running:
@@ -72,12 +74,18 @@ class Game(object):
                     sleep(0.5)
                     self.toggle()
 
-    def comp_stop(self):
+    def stop(self):
         self.running = False
+        self.display.stop()
+        sleep(0.5)
+        IO.cleanup()
 
 
-
-IO.setmode(IO.BOARD)
-display = Display()
-game = Game(display)
-game.start()
+if __name__ == '__main__':
+    IO.setmode(IO.BOARD)
+    display = Display()
+    game = Game(display, sys.argv[1])
+    try:
+        game.start()
+    except KeyboardInterrupt:
+        game.stop()
